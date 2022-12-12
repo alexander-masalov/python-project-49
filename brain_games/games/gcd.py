a0000693cee226c8b932634241164dd62512e631
alexander-masalov <masalenok477@gmail.com>
2022-12-12 17:57:40 +0300

    edit pyproject.toml and add gcd.py, brain_gcd.py

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,12 @@
+import random
+import math
+
+
+rules = 'Find the greatest common divisor of given numbers.'
+
+
+def question_generate():
+    number_1 = random.randint(1, 99)
+    number_2 = random.randint(1, 99)
+    return f'{number_1} {number_2}', math.gcd(number_1, number_2)
+    
