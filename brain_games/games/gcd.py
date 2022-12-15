d42b17f3786624bf2315ca0629bc971d281397c7
alexander-masalov <masalenok477@gmail.com>
2022-12-15 23:00:01 +0300

    Добавил константы в gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -2,10 +2,12 @@ import random
 import math
 
 
-rules = 'Find the greatest common divisor of given numbers.'
+RULES = 'Find the greatest common divisor of given numbers.'
+MIN_LIMIT = 1
+MAX_LIMIT = 100
 
 
 def question_generate():
-    number_1 = random.randint(1, 99)
-    number_2 = random.randint(1, 99)
+    number_1 = random.randint(MIN_LIMIT, MAX_LIMIT)
+    number_2 = random.randint(MIN_LIMIT, MAX_LIMIT)
     return f'{number_1} {number_2}', math.gcd(number_1, number_2)
