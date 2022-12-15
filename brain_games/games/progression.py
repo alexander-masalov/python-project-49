6e872e1658d898c4dbc5ab3b9e8bd51a777ef855
alexander-masalov <masalenok477@gmail.com>
2022-12-15 23:01:39 +0300

    Добавил константы

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,17 +1,24 @@
 import random
 
 
-rules = 'What number is missing in the progression?'
+RULES = 'What number is missing in the progression?'
+MIN_START = 1
+MAX_START = 20
+STOP = 100
+MIN_STEP = 1
+MAX_STEP = 9
+MIN_LEN = 5
+MAX_LEN = 10
 
 
 def question_generate():
-    start = random.randint(1, 20)
-    stop = 100
-    step = random.randint(1, 9)
+    start = random.randint(MIN_START, MAX_START)
+    step = random.randint(MIN_STEP, MAX_STEP)
+    len_progression = random.randint(MIN_LEN, MAX_LEN)
     progression = []
-    for item in range(start, stop, step):
+    for item in range(start, STOP, step):
         progression.append(str(item))
-        if len(progression) > random.randint(5, 10):
+        if len(progression) > len_progression:
             break
     random_index = random.randint(0, len(progression) - 1)
     correct_answer = progression[random_index]
