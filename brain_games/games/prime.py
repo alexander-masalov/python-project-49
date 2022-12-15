8bc80b16f836ce016a0b9d3c48592509fc0934d7
alexander-masalov <masalenok477@gmail.com>
2022-12-15 23:00:58 +0300

    Добавил константы, функцию is_prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,12 +1,21 @@
 import random
 
 
-rules = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+MIN_LIMIT = 1
+MAX_LIMIT = 100
+
+
+def is_prime(x):
+    for i in range(2, x):
+        if x % i == 0:
+            return False
+    return True
 
 
 def question_generate():
-    number = random.randint(2, 100)
-    for item in range(2, number):
-        if number % item == 0:
-            return number, 'no'
-    return number, 'yes'
+    number = random.randint(MIN_LIMIT, MAX_LIMIT)
+    if is_prime(number):
+        return number, 'yes'
+    else:
+        return number, 'no'
