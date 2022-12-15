7fd4fecc67c91700e5cb2692946021aa68ba73f1
alexander-masalov <masalenok477@gmail.com>
2022-12-15 22:58:23 +0300

    Добавил константы, функцию is_even

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,12 +1,21 @@
 import random
 
 
-rules = 'Answer "yes" if the number is even, otherwise answer "no".'
+RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
+MIN_LIMIT = 1
+MAX_LIMIT = 100
+
+
+def is_even(x):
+    if x % 2 == 0:
+        return True
+    else:
+        return False
 
 
 def question_generate():
-    number = random.randint(1, 99)
-    if number % 2 == 0:
+    number = random.randint(MIN_LIMIT, MAX_LIMIT)
+    if is_even(number):
         return number, 'yes'
-    elif number % 2 != 0:
+    else:
         return number, 'no'
