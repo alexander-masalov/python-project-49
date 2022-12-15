2518267a74e9ec1ca4ca802ce88b8f13159f423f
alexander-masalov <masalenok477@gmail.com>
2022-12-15 22:59:14 +0300

    Добавил константы

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,14 +1,16 @@
 import random
 
 
-rules = "What is the result of the expression?"
+RULES = "What is the result of the expression?"
+MIN_LIMIT = 1
+MAX_LIMIT = 100
 
 
 def question_generate():
     operations = ['+', '-', '*']
     operation = random.choice(operations)
-    number_1 = random.randint(1, 99)
-    number_2 = random.randint(1, 99)
+    number_1 = random.randint(MIN_LIMIT, MAX_LIMIT)
+    number_2 = random.randint(MIN_LIMIT, MAX_LIMIT)
     if operation == '+':
         return f'{number_1} + {number_2}', number_1 + number_2
     elif operation == '-':
